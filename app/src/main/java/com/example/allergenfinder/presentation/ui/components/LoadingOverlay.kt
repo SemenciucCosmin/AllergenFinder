01aa9ebfac1d3d7a2fd36324309013184ba3da58
Semenciuc Cosmin <cosmin.semenciuc@garmin.com>
2023-12-20 23:37:48 +0200

    Add LoadingOverlay

---
+++ app/src/main/java/com/example/allergenfinder/presentation/ui/components/LoadingOverlay.kt
@@ -0,0 +1,48 @@
+package com.example.allergenfinder.presentation.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.allergenfinder.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun LoadingOverlay() {
+    var angle by remember { mutableFloatStateOf(0f) }
+
+    LaunchedEffect(angle) {
+        delay(60)
+        angle += 30f
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White.copy(alpha = 0.2f))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_loading),
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .rotate(angle)
+        )
+    }
+}
